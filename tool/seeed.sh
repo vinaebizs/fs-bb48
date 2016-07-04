a4c785718735ab7dc492c9ff90fb797f71c27272
NIIBE Yutaka <gniibe@fsij.org>
2016-07-04 11:52:21 +0900

    Fix file extentions for newer KiCAD

--- tool/seeed.sh
+++ tool/seeed.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
-cp -p fs-bb48-F_Cu.gbr FS-BB48.GTL
-cp -p fs-bb48-F_Mask.gbr FS-BB48.GTS
-cp -p fs-bb48-F_SilkS.gbr FS-BB48.GTO
+cp -p fs-bb48-F.Cu.gbr FS-BB48.GTL
+cp -p fs-bb48-F.Mask.gbr FS-BB48.GTS
+cp -p fs-bb48-F.SilkS.gbr FS-BB48.GTO
 
-cp -p fs-bb48-B_Cu.gbr FS-BB48.GBL
-cp -p fs-bb48-B_Mask.gbr FS-BB48.GBS
-cp -p fs-bb48-B_SilkS.gbr FS-BB48.GBO
+cp -p fs-bb48-B.Cu.gbr FS-BB48.GBL
+cp -p fs-bb48-B.Mask.gbr FS-BB48.GBS
+cp -p fs-bb48-B.SilkS.gbr FS-BB48.GBO
 
-cp -p fs-bb48-Edge_Cuts.gbr FS-BB48.GML
+cp -p fs-bb48-Edge.Cuts.gbr FS-BB48.GML
 cp -p fs-bb48.drl FS-BB48.TXT
 
 # -l: translate LF to CRLF
